9bb2d7420a98a811f5fb81668c67b656831f783f
LaxxNacken <j.s.laxxnacken@gmail.com>
2023-12-03 14:06:54 +0100

    2023-12-03     -finished win logic     -added different set modes     -first real styling added

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # volleyball-counter
 A volleyball counter I made for my volleyball club
 
+# To-Do's
+    -Proper style, asking Simon about how to style it 
+    -Additional, asking Simon about which features are needed
+
 # 2023-12-02
     -created repo
     -added files
@@ -8,3 +12,8 @@ A volleyball counter I made for my volleyball club
     -added background color
     -added base construct in html
     -added some win logic
+
+# 2023-12-03
+    -finished win logic
+    -added different set modes
+    -first real styling added
--- script.js
+++ script.js
@@ -4,12 +4,35 @@ document.addEventListener('DOMContentLoaded', function() {
     //HTML Elements
     buttonTeamA = document.getElementById("button-team-a");
     buttonTeamB = document.getElementById("button-team-b");
+    textWinnerMessage = document.getElementById("winner-message");
+    buttonStandardSet = document.getElementById("standard-set");
+    buttonSmallSet = document.getElementById("small-set");
+    divTeamA = document.getElementById("div-team-a");
+    divTeamB = document.getElementById("div-team-b");
 
     //Generic Variables
     scoreTeamA = 0;
     scoreTeamB = 0;
+    setType = "";
 
+    //Ensure the right text ("0") at the start
     updateButtonText();
+
+    //Selecting Set Mode
+    buttonStandardSet.addEventListener("click", function() {
+        divTeamA.classList.remove("hide-element");
+        divTeamB.classList.remove("hide-element");
+        buttonStandardSet.classList.add("hide-element");
+        buttonSmallSet.classList.add("hide-element");
+        setType = "standard";
+    });
+    buttonSmallSet.addEventListener("click", function() {
+        divTeamA.classList.remove("hide-element");
+        divTeamB.classList.remove("hide-element");
+        buttonStandardSet.classList.add("hide-element");
+        buttonSmallSet.classList.add("hide-element");
+        setType = "small";
+    });
 
     //Score Logic
     buttonTeamA.addEventListener("click", function() {
@@ -22,11 +45,24 @@ document.addEventListener('DOMContentLoaded', function() {
         updateButtonText();
         checkWinConditions();
     });
-    
+
     //Checks if any win condition applies
     function checkWinConditions(){
-        if(scoreTeamA >= 25 && scoreTeamB < scoreTeamA - 2){
-            console.log("Team A wins!")
+        if (setType == "standard") {
+            if(scoreTeamA >= 25 && scoreTeamB < scoreTeamA - 1){
+                textWinnerMessage.textContent = "Team A wins!";
+            }
+            else if(scoreTeamB >= 25 && scoreTeamA < scoreTeamB - 1){
+                textWinnerMessage.textContent = "Team B wins!";
+            }
+        }
+        if (setType == "small") {
+            if(scoreTeamA >= 15 && scoreTeamB < scoreTeamA - 1){
+                textWinnerMessage.textContent = "Team A wins!";
+            }
+            else if(scoreTeamB >= 15 && scoreTeamA < scoreTeamB - 1){
+                textWinnerMessage.textContent = "Team B wins!";
+            }
         }
     }
 
@@ -35,4 +71,5 @@ document.addEventListener('DOMContentLoaded', function() {
         buttonTeamA.textContent = scoreTeamA;
         buttonTeamB.textContent = scoreTeamB;
     }
+
 });
